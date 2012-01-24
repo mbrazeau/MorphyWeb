9e8d99afc91fad8f1680c3ad9c5b62b74ed57e35
Chris Desjardins <git@chrisd.info>
2012-01-24 22:06:03 +0100

    Fill out the rest of the menu.

--- nui/nui.cpp
+++ nui/nui.cpp
@@ -1,8 +1,19 @@
 #include "nui.h"
 
+bool CNexusMenuSpacer::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    return true;
+}
+
 bool CNexusMenuOpenFile::MenuFunction(CNexusUserInterface *pNexusUserInterface)
 {
     cout<<"OPEN FILE STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuSaveFile::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"SAVE FILE STILL NEEDS TO BE IMPLEMENTED"<<endl;
     return true;
 }
 
@@ -24,12 +35,144 @@ bool CNexusMenuQuit::MenuFunction(CNexusUserInterface *pNexusUserInterface)
     return false;
 }
 
+bool CNexusMenuAbout::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"ABOUT STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuLog::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"LOG STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuStatus::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"STATUS STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuChdir::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"CHDIR STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuExclude::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"EXCLUDE STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuInclude::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"INCLUDE STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuOutgroup::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"OUTGROUP STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuIngroup::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"INGROUP STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuChar::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"CHAR STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+                         
+bool CNexusMenuHeuristicSearch::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"HEURISTIC SEARCH STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuExhaust::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"EXHAUST STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuBNB::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"BNB STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuBootstrap::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"BOOTSTRAP STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuJackknife::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"JACKKNIFE STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuSTR::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"STR STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+                         
+bool CNexusMenuConsens::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"CONSENS STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+bool CNexusMenuCollapse::MenuFunction(CNexusUserInterface *pNexusUserInterface)
+{
+    cout<<"COLLAPSE STILL NEEDS TO BE IMPLEMENTED"<<endl;
+    return true;
+}
+
+
 CNexusUserInterface::CNexusUserInterface()
 {
-    m_vMenu.push_back(new CNexusMenuOpenFile  ("O", "Open a file"));
-    m_vMenu.push_back(new CNexusMenuCloseFile ("C", "Close a file"));
-    m_vMenu.push_back(new CNexusMenuHelp      ("H", "Help"));
-    m_vMenu.push_back(new CNexusMenuQuit      ("Q", "Quit"));
+    m_vMenu.push_back(new CNexusMenuSpacer      (NULL, "File"));
+    m_vMenu.push_back(new CNexusMenuOpenFile        ("O", "Open a file"));
+    m_vMenu.push_back(new CNexusMenuCloseFile       ("C", "Close a file"));
+    m_vMenu.push_back(new CNexusMenuSaveFile        ("S", "Save according to the options"));
+
+    m_vMenu.push_back(new CNexusMenuSpacer      (NULL, "Program"));
+    m_vMenu.push_back(new CNexusMenuLog             ("LOG" ,"Toggles record of commands, variable states, etc"));
+    m_vMenu.push_back(new CNexusMenuStatus          ("STAT","Prints status of all current settings, eg. logmode on/off"));
+    m_vMenu.push_back(new CNexusMenuChdir           ("CD"  ,"change working directory"));
+    m_vMenu.push_back(new CNexusMenuHelp            ("H"   , "Help"));
+    m_vMenu.push_back(new CNexusMenuAbout           ("A"   , "About"));
+    m_vMenu.push_back(new CNexusMenuQuit            ("Q"   , "Quit"));
+
+    m_vMenu.push_back(new CNexusMenuSpacer      (NULL, "Data"));
+    m_vMenu.push_back(new CNexusMenuExclude         ("EXC" , "Exclude taxa or characters"));
+    m_vMenu.push_back(new CNexusMenuInclude         ("INC" , "Include excluded taxa or characters"));
+    m_vMenu.push_back(new CNexusMenuOutgroup        ("OUTG", "Assign taxa to outgroup"));
+    m_vMenu.push_back(new CNexusMenuIngroup         ("ING" , "Return taxa from outgroup to ingrou"));
+    m_vMenu.push_back(new CNexusMenuChar            ("CHAR", "Modify a character's type"));
+
+    m_vMenu.push_back(new CNexusMenuSpacer      (NULL, "Analysis"));
+    m_vMenu.push_back(new CNexusMenuHeuristicSearch ("HS" , "Begin a heuristic search"));
+    m_vMenu.push_back(new CNexusMenuExhaust         ("EXS", "Begin an exhaustive search"));
+    m_vMenu.push_back(new CNexusMenuBNB             ("BNB", "Begin a branch-and-bound search"));
+    m_vMenu.push_back(new CNexusMenuBootstrap       ("BTS", "Begin a bootstrap analysis"));
+    m_vMenu.push_back(new CNexusMenuJackknife       ("JK" , "Begin a jackknife analysis"));
+    m_vMenu.push_back(new CNexusMenuSTR             ("STR", "Perform a safe taxonomic reduction"));
+
+    m_vMenu.push_back(new CNexusMenuSpacer      (NULL, "Results"));
+    m_vMenu.push_back(new CNexusMenuConsens         ("CONSENS" , "Compute consensus tree for trees in memory"));
+    m_vMenu.push_back(new CNexusMenuCollapse        ("COLLAPSE", "Collapse zero-length branches, condense the tree set"));
 }
 
 CNexusUserInterface::~CNexusUserInterface()
--- nui/nui.h
+++ nui/nui.h
@@ -10,14 +10,24 @@ class CNexusMenuBase
 public:
     CNexusMenuBase(const char * strCommand, const char * strHelpText)
     {
-        m_strCommand = strCommand;
+        if (strCommand)
+        {
+            m_strCommand = strCommand;
+        }
         m_strHelpText = strHelpText;
         transform(m_strCommand.begin(), m_strCommand.end(), m_strCommand.begin(), ::toupper);
     }
 
     string GetMenuOutput()
     {
-        return m_strCommand + ") " + m_strHelpText;
+        if (m_strCommand.length() > 0)
+        {
+            return " " + m_strCommand + ") " + m_strHelpText;
+        }
+        else
+        {
+            return "\n=== " + m_strHelpText;
+        }
     }
 
     bool IsSelection(string strInput)
@@ -40,10 +50,34 @@ private:
         bool MenuFunction(CNexusUserInterface *pNexusUserInterface);\
     };
 
-NEW_COMMAND_DEFINE(CNexusMenuOpenFile)
-NEW_COMMAND_DEFINE(CNexusMenuCloseFile)
-NEW_COMMAND_DEFINE(CNexusMenuHelp)
-NEW_COMMAND_DEFINE(CNexusMenuQuit)
+NEW_COMMAND_DEFINE(CNexusMenuSpacer         )
+                                            
+NEW_COMMAND_DEFINE(CNexusMenuOpenFile       )
+NEW_COMMAND_DEFINE(CNexusMenuSaveFile       )
+NEW_COMMAND_DEFINE(CNexusMenuCloseFile      )
+                                            
+NEW_COMMAND_DEFINE(CNexusMenuHelp           )
+NEW_COMMAND_DEFINE(CNexusMenuQuit           )
+NEW_COMMAND_DEFINE(CNexusMenuAbout          )
+NEW_COMMAND_DEFINE(CNexusMenuLog            )
+NEW_COMMAND_DEFINE(CNexusMenuStatus         )
+NEW_COMMAND_DEFINE(CNexusMenuChdir          )
+                                            
+NEW_COMMAND_DEFINE(CNexusMenuExclude        )
+NEW_COMMAND_DEFINE(CNexusMenuInclude        )
+NEW_COMMAND_DEFINE(CNexusMenuOutgroup       )
+NEW_COMMAND_DEFINE(CNexusMenuIngroup        )
+NEW_COMMAND_DEFINE(CNexusMenuChar           )
+
+NEW_COMMAND_DEFINE(CNexusMenuHeuristicSearch)
+NEW_COMMAND_DEFINE(CNexusMenuExhaust        )
+NEW_COMMAND_DEFINE(CNexusMenuBNB            )
+NEW_COMMAND_DEFINE(CNexusMenuBootstrap      )
+NEW_COMMAND_DEFINE(CNexusMenuJackknife      )
+NEW_COMMAND_DEFINE(CNexusMenuSTR            )
+                                            
+NEW_COMMAND_DEFINE(CNexusMenuConsens        )
+NEW_COMMAND_DEFINE(CNexusMenuCollapse       )
 
 class CNexusUserInterface
 {
