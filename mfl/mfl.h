ae703c364e6e515d3e3a38a132ddeed9ffa9c55f
Martin Brazeau <martin.brazeau@gmail.com>
2012-01-27 17:19:46 +0100

    +mfl_collapose prototype

--- mfl/mfl.h
+++ mfl/mfl.h
@@ -7,13 +7,16 @@
  *
  */
 
-/* The underlying model here is that a datafile has been opened, parsed, and 
+/* 
+ * The underlying model here is that a datafile has been opened, parsed, and 
  * read. mfl_analysis_environment has a series of variables within it set 
  * according to the file data or some values calculated based on that. From 
  * here, it's possible to call any number of the subsequent functions relating 
  * to data analysis and tree searching. Some of the parameters would come from
  * command-line input. Others would be values available within the scope of 
- * mfl_analysis_environment, set either by the input file data or by functions*/
+ * mfl_analysis_environment, set either by the input file data or by functions
+ *
+ */
 
 mfl_analysis_environment(data_from_input_file); // When morphy has data from the reader, this starts up otherwise, the user gets an error msg that there's no active data file
 mfl_set_excludes(taxa_to_exclude, chars_to_exclude); // Optionally remove some taxa or characters from the analysis.
@@ -29,6 +32,7 @@ mfl_jackknife(int ntax, int numnodes, int nchar, int *list_of_included taxa, int
 mfl_safe_taxonomic_reduction(list_of_ordered_characters);
 mfl_ratchet(n_chars_to_perturb, reweightorjackknife);   // A type of super-fast heuristic search, but is subordinate to mfl_heuristic
 mfl_consensus(tree **savedtrees, bool strict, bool majority_rule, int majority_rule_optional_cutoff, int adams);
+mfl_collapse(during_search, if_min_zero_length, if_max_zero_length);
 mfl_set_treelimit(int max_number_of_trees_in_mem, tree **savedtrees);
 mfl_save_trees_to_file(tree **savedtrees); // Depends on there being trees in memory
 mfl_show_all_trees(tree **savedtrees); // Prints out all the trees in memory, if any
