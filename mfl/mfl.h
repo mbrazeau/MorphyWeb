9d245966ac7fbd4b8347b747b8d99ca7dbfcbf73
Martin Brazeau <martin.brazeau@gmail.com>
2012-01-29 13:35:17 +0100

    some additional comments

--- mfl/mfl.h
+++ mfl/mfl.h
@@ -23,6 +23,13 @@ typedef enum
 mfl_handle_t* mfl_create_handle();
 void mfl_destroy_handle(mfl_handle_t* mfl_handle);
 
+/* MDB: Now that I see some of this new code, I think I now get what the 
+ * problem with these functions is. Indeed, the lists of included and excluded 
+ * characters and taxa are effectively parameters in any of the analyses. If I
+ * understand this correctly, a command to change the taxon/character inclusion
+ * would just result in a new mfl_handle with all the appropriate set parameters
+ */
+
 #ifdef MFL_EXCLUDES
 mfl_set_excludes(mfl_handle_t*, taxa_to_exclude, chars_to_exclude); // Optionally remove some taxa or characters from the analysis.
 mfl_set_includes(mfl_handle_t*, taxa_to_include, chars_to_include); // Optionally reinstate removed taxa or characters to the analysis
@@ -49,6 +56,15 @@ string mfl_get_trees(mfl_handle_t* mfl_handle);
  * Which pulls all of the trees out of the mfl_handle and formats them into a string ready for output
  * to the user, then the interface can just directly write the string to the screen, or to a file, etc...
  * What other use cases are there?
+ *
+ * MDB: Perhaps they return a struct mfl_results*. Contains the relevant data
+ * such as the length of the shortest tree(s), number of trees save, number of 
+ * rearrangments tried (where relevant), a pointer to the list of saved
+ * trees, etc. We need to save the tree structs because the user may choose to 
+ * compute consensus trees and only save those (especially if > 15 trees were 
+ * saved) or they'll want compute full ancestral states reconstructions. These will
+ * require the original tree structs in memory.
+ *
  */
 mfl_heuristic       (mfl_handle_t* mfl_handle);
 mfl_exhaustive      (mfl_handle_t* mfl_handle);
@@ -74,6 +90,10 @@ mfl_collapse_zerolength(during_search, if_min_zero_length, if_max_zero_length);
  * Or in mfl_handle? in which case the mfl_clear_treebuffer function just takes mfl_handle_t* mfl_handle as a parameter.
  * Additionally if we are going to make tree a part of the API, then it should really be called mfl_tree... but I am not 100%
  * sure that we need to export the tree datastruct...
+ *
+ * MDB: Right. These are probably both 'behind the scenes'. Not sure how
+ * numsavedtrees could be stored usefully in the tree struct. More likely in 
+ * mfl_handle (or even in the results struct I suggested).
  */
 /*-Already written-*/
 void mfl_resize_treebuffer(tree **treebuffer, int *treelimit, int sizeincrease); // Called if the user gives a command to store more than the current/default number of trees
