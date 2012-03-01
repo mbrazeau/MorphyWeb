a1c95e4abb91081db3a6ae91ac1c464e738be55a
Martin Brazeau <martin.brazeau@gmail.com>
2012-03-01 14:55:33 +0100

    +functions for cases of mfl_set_parameter

--- mfl/mfyinterface.c
+++ mfl/mfyinterface.c
@@ -19,6 +19,133 @@ void mfl_destroy_handle(mfl_handle_t *mfl_handle)
     free(mfl_handle);
 }
 
+void mfl_set_ntax(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->n_taxa = (int)(*param_data);
+}
+
+void mfl_set_nchar(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->n_chars = (int)(*param_data);
+}
+
+void mfl_set_searchtype(mfl_handle_t *mfl_handle, void *param_data)
+{
+    switch (*param_data) { //should *param_data be type-cast?
+        case 0:
+            // Exhaustive
+            mfl_handle->search_type = 0;
+            break;
+        case 1:
+            // Branch-and-bound
+            mfl_handle->search_type = 1;
+            break;
+        case 2:
+            // Heuristic
+            mfl_handle->search_type = 2;
+            break;
+        default:
+            //Return an error
+            break;
+    }
+}
+
+void mfl_set_numiterations(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->n_iterations = (int)(*param_data);
+}
+
+void mfl_set_treelimit(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->n_treelimit = (int)(*param_data);
+}
+
+void mfl_set_branchswap_t(mfl_handle_t *mfl_handle, void *param_data)
+{
+    switch (*param_data) { //should *param_data be type-cast?
+        case 0:
+            // TBR
+            mfl_handle->bswap_type = 0;
+            break;
+        case 1:
+            // SPR
+            mfl_handle->bswap_type = 1;
+            break;
+        case 2:
+            // NNI
+            mfl_handle->bswap_type = 2;
+            break;
+        default:
+            //Return an error
+            break;
+    }
+}
+
+void mfl_set_ratchet_status(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->is_ratchet = (bool)(*param_data);
+}
+
+void mfl_attach_inputdata(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->input_data = (char *)param_data;
+}
+
+void mfl_set_addseq_t(mfl_handle_t *mfl_handle, void *param_data)
+{
+    switch (*param_data) { //should *param_data be type-cast?
+        case 0:
+            // As-is
+            mfl_handle->addseq_type = 0;
+            break;
+        case 1:
+            // Random
+            mfl_handle->addseq_type = 1;
+            break;
+        case 2:
+            // Simple
+            // Not implemented yet
+            //mfl_handle->addseq_type = 2;
+            break;
+        case 3:
+            // Closest
+            // Not implemented yet
+            // mfl_handle->addseq_type = 3;
+            break;
+
+        default:
+            //Return an error
+            break;
+    }
+    
+}
+
+void mfl_set_collapse(mfl_handle_t *mfl_handle, void *param_data)
+{
+    mfl_handle->collapse_nolen = (bool)(*param_data);
+}
+
+void mfl_set_collapse_value(mfl_handle_t *mfl_handle, void *param_data)
+{
+    switch (*param_data) { //should *param_data be type-cast?
+        case 0:
+            // Max length 0
+            mfl_handle->collapse_at = 0;
+            break;
+        case 1:
+            // Min length 0
+            mfl_handle->collapse_at = 1;
+            break;
+        case 2:
+            // Equal reconstruction sets
+            mfl_handle->collapse_at = 3;
+            break;
+        default:
+            //Return an error
+            break;
+    }
+}
+
 bool mfl_set_parameter(mfl_handle_t *mfl_handle, mfl_param_t param_type, void *param_data)
 {
     
