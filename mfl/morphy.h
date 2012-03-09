f51f6753437ee28f4b3cabbee642be09cc4ba2a3
mbrazeau <martin.brazeau@gmail.com>
2012-03-09 14:27:57 +0100

    +function headers

--- mfl/morphy.h
+++ mfl/morphy.h
@@ -318,7 +318,26 @@ void mfl_bisection_traversal(node *n, tree *swapingon, tree **savedtrees, int nt
                              int nchar, int numnodes, mfl_searchrec *searchrec, long int *current, bool *undertreelimit, 
                              int *currentbesttree, bool *foundbettertree, bool *success, long int *leftotry);
 
+/* in mfyinterface.c*/
+mfl_handle_t* mfl_create_handle(void);
+void mfl_destroy_handle(mfl_handle_t *mfl_handle);
+bool mfl_set_ntax(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_nchar(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_searchtype(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_numiterations(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_treelimit(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_branchswap_t(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_ratchet_status(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_attach_inputdata(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_addseq_t(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_collapse(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_collapse_value(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_gapormissing(mfl_handle_s *mfl_struct, void *param_data);
+bool mfl_set_parameter(mfl_handle_t *mfl_handle, mfl_param_t param_type, void *param_data);
+mfl_resultant_data_s *mfl_morphy_controller(mfl_handle_s *mfl_handle);
 mfl_handle_t *mfl_s2t(mfl_handle_s *mfl_handle);
 mfl_handle_s *mfl_t2s(mfl_handle_t *mfl_handle);
+
+
 
 /*End function prototypes*/
