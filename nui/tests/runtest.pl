092c0bfc73ee66f8fd4546cbe66276dea1bbf8a9
Chris Desjardins <git@chrisd.info>
2012-04-26 11:51:59 +0200

    Small updates to test runner

--- nui/tests/runtest.pl
+++ nui/tests/runtest.pl
@@ -5,9 +5,11 @@ use Getopt::Long;
 my $g_input_file;
 my $g_expected_output_file;
 my $g_test_dir;
+my $g_update_dirs;
 GetOptions( "input=s" => \$g_input_file,
             "expected=s" => \$g_expected_output_file,
-            "test=s" => \$g_test_dir);
+            "test=s" => \$g_test_dir,
+            "update:s" => \$g_update_dirs);
 
 sub execute_nui(@)
 {
@@ -24,15 +26,28 @@ sub read_expected_results(@)
     close FILE;
 }
 
+sub get_tests_to_update(@)
+{
+    my $tests_to_update;
+    my @tests = split(/,/, $g_update_dirs);
+    foreach my $test (@tests)
+    {
+        $tests_to_update->{$test} = 1;
+    }
+    return $tests_to_update;
+}
+
 sub runtest(@)
 {
     my ($input_file, $expected_output_file, $test_dir) = @_;
     my $actual_results = execute_nui($input_file);
+    my $tests_to_update = get_tests_to_update();
+    my $actual_output_file = "$test_dir/actual.out";
 
     open(OUTPUTF, ">$test_dir/actual.out");
     print OUTPUTF "$actual_results";
     close(OUTPUTF);
-    my $diffresults = `diff $test_dir/actual.out $expected_output_file`;
+    my $diffresults = `diff $actual_output_file $expected_output_file`;
     if (length($diffresults) == 0)
     {
         print ("$test_dir - pass\n");
@@ -40,6 +55,12 @@ sub runtest(@)
     else
     {
         print("\n\n\n************* $test_dir FAILURE - $diffresults\n");
+        if ($tests_to_update->{$test_dir})
+        {
+            print("Updating test: $test_dir\n\n");
+            `cp $expected_output_file $expected_output_file.prev`;
+            `cp $actual_output_file $expected_output_file`;
+        }
     }
     return 0;
 }
