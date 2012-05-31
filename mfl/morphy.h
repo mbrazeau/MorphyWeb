88810f31a125f11463665fc4a84eb09a3f102bb1
Martin Brazeau <martin.brazeau@gmail.com>
2012-05-31 16:46:18 +0200

    +function prototypes for new hsearch fns

--- mfl/morphy.h
+++ mfl/morphy.h
@@ -321,9 +321,11 @@ long int mfl_spr_leftotry(int ntax);
 void (*mfl_swap_controller(mfl_handle_s *mfl_handle)) (node*, tree*, tree**, int, int , int, mfl_searchrec*);
 void mfl_regrafting_traversal(node *n, node *subtr, tree *swapingon, tree **savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec *searchrec, int diff);
 void mfl_pruning_traversal(node *n, tree *swapingon, tree **savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec *searchrec);
-void mfl_reroot_subtree(node *n, node *subtr, node *base/*, tree swapingon, tree *savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec *searchrec*/);
+void mfl_reroot_subtree(node *n, node *subtr, node *base, tree *swapingon, tree **savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec *searchrec);
 bool mfl_heuristic_search(mfl_handle_s *mfl_handle/*int ntax, int nchar, int numnodes, char *txtsrcdata, tree **savedtrees, int starttreelen*/);
 void mfl_pruning_traversal_ii(node *n, tree *swapingon, tree **savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec* searchrec);
+void mfl_spr_cliptrees(node *p, node *up, node *dn, node *subtr, tree *swapingon, tree **savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec *searchrec);
+void mfl_reroot_subtree(node *n, node *subtr, node *base, node *up, node *dn, tree *swapingon, tree **savedtrees, int ntax, int nchar, int numnodes, mfl_searchrec *searchrec);
 
 /* in mfyinterface.c*/
 bool mfl_set_ntax(mfl_handle_s *mfl_struct, void *param_data);
