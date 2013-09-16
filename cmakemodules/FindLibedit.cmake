73e0e47e377754d2ba56cc7a283d5f81e5109142
Chris Desjardins <chris.desjardins@amscins.com>
2013-09-16 18:25:06 +0200

    Make the cmake errors easier to debug

--- cmakemodules/FindLibedit.cmake
+++ cmakemodules/FindLibedit.cmake
@@ -1,14 +1,24 @@
-find_path(PATH_INC_EDIT NAMES "histedit.h" PATHS)
+set(EDIT_INC_FILE "histedit.h")
+set(EDIT_LIB_FILE "libedit.a")
+
+find_path(PATH_INC_EDIT NAMES ${EDIT_INC_FILE} PATHS env PATH_INC_EDIT)
+
+macro(editerr filename envvar)
+    message(FATAL_ERROR
+    "Unable to locate libedit: '${filename}' "
+    "Either install libedit to a standard location or set the "
+    "env variable '${envvar}' to the location of '${filename}'")
+endmacro()
 
 if( NOT PATH_INC_EDIT )
-        message(FATAL_ERROR"Unable to locate libedit include files" )
-endif( NOT PATH_INC_EDIT )
+    editerr(${EDIT_INC_FILE} "PATH_INC_EDIT")
+endif()
 
-find_library(PATH_LIB_EDIT NAMES "libedit.a" PATHS)
+find_library(PATH_LIB_EDIT NAMES ${EDIT_LIB_FILE} PATHS ENV PATH_LIB_EDIT)
 
 if( NOT PATH_LIB_EDIT )
-        message(FATAL_ERROR "Unable to locate libedit library file" )
-endif( NOT PATH_LIB_EDIT )
+    editerr(${EDIT_LIB_FILE} "PATH_LIB_EDIT")
+endif()
 
-MESSAGE( STATUS "PATH_INC_EDIT = \"${PATH_INC_EDIT}\"" )
-MESSAGE( STATUS "PATH_LIB_EDIT = \"${PATH_LIB_EDIT}\"" )
+message(STATUS "PATH_INC_EDIT = \"${PATH_INC_EDIT}\"")
+message(STATUS "PATH_LIB_EDIT = \"${PATH_LIB_EDIT}\"")
--- cmakemodules/FindNcl.cmake
+++ cmakemodules/FindNcl.cmake
@@ -1,14 +1,24 @@
-find_path(PATH_INC_NCL NAMES "ncl/ncl.h" PATHS)
+set(NCL_INC_FILE "ncl/ncl.h")
+set(NCL_LIB_FILE "ncl/libncl.a")
+
+find_path(PATH_INC_NCL NAMES ${NCL_INC_FILE} PATHS env PATH_INC_NCL)
+
+macro(nclerr filename envvar)
+    message(FATAL_ERROR
+    "Unable to locate Nexus Class Library: '${filename}' "
+    "Either install NCL to a standard location or set the "
+    "env variable '${envvar}' to the location of '${filename}'")
+endmacro()
 
 if( NOT PATH_INC_NCL )
-        message(FATAL_ERROR "Unable to locate Nexus Class Library include files" )
-endif( NOT PATH_INC_NCL )
+    nclerr(${NCL_INC_FILE} "PATH_INC_NCL")
+endif()
 
-find_library(PATH_LIB_NCL NAMES "ncl/libncl.a" PATHS)
+find_library(PATH_LIB_NCL NAMES ${NCL_LIB_FILE} PATHS ENV PATH_LIB_NCL)
 
 if( NOT PATH_LIB_NCL )
-        message(FATAL_ERROR "Unable to locate Nexus Class Library file" )
-endif( NOT PATH_LIB_NCL )
+    nclerr(${NCL_LIB_FILE} "PATH_LIB_NCL")
+endif()
 
-MESSAGE( STATUS "PATH_INC_NCL = \"${PATH_INC_NCL}\"" )
-MESSAGE( STATUS "PATH_LIB_NCL = \"${PATH_LIB_NCL}\"" )
+message(STATUS "PATH_INC_NCL = \"${PATH_INC_NCL}\"")
+message(STATUS "PATH_LIB_NCL = \"${PATH_LIB_NCL}\"")
