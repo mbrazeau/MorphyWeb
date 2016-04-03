0884163580a37fd007a8c27f38710d1db41c709c
mbrazeau <mbrazeau@mbrazeaus-MBP.mshome.net>
2016-04-03 16:01:40 +0200

    free_treenodes() stops on NULL terminal of nodearr

--- mfl/mfl_tree.c
+++ mfl/mfl_tree.c
@@ -177,7 +177,7 @@ void mfl_free_treenodes(mfl_nodearray_t treenodes, int num_nodes)
 {
     int i = 0;
     
-    for (i = 0; i < num_nodes; ++i) {
+    for (i = 0; *treenodes; ++i) {
         if ((*treenodes)->nodet_next) {
             mfl_destroy_n_nary_ring(*treenodes);
         }
