e62908e29fefa9f23a39da8dd3ac93727c2e0fc1
mbrazeau <mbrazeau@mbrazeaus-mbp.home>
2016-04-07 18:06:08 +0100

    tidied up fn names and comments

--- mfl/mfl_characters.c
+++ mfl/mfl_characters.c
@@ -10,19 +10,6 @@
 
 #include "morphy.h"
 
-/*typedef struct mfl_nodedata_t {
-    int nd_n_characters;                        // The number of characters within the datablock.
-    int nd_n_taxa;                              // The number of taxa the datablock applies to.
-    mfl_optimisation_t nd_optimisation_method;  // The optimisation method applied to all characters in this datablock.
-    bool nd_inapplicables;                      // false: no inapplicables; true: has inapplicables.
-    mfl_costs_t *nd_costmatrix;                 // Cost matrix associated with these characters.
-    mfl_parsim_fn nd_downpass;                  // The downpass parsimony function
-    mfl_parsim_fn nd_uppass;                    // The uppass parsimony function
-    charstate *nd_prelim_set;                 // The characters to which the datablock applies.
-    charstate *nd_final_set;
-    charstate *nd_subtree_prelim_set;
-    charstate *nd_subtree_final_set;
-} mfl_nodedata_t;*/
 
 mfl_nodedata_t* mfl_alloc_datapart(void)
 {
@@ -42,7 +29,7 @@ mfl_nodedata_t* mfl_alloc_datapart(void)
 }
 
 
-void mfl_free_datapart(mfl_nodedata_t *olddata)
+void mfl_free_nodedata(mfl_nodedata_t *olddata)
 {
     if (olddata->nd_prelim_set) {
         free(olddata->nd_prelim_set);
