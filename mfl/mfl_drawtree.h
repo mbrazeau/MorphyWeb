6d2f8387ab639dcb10a380bfbf3f0c5a300ef764
mbrazeau <mbrazeau@dyn1208-62.wlan.ic.ac.uk>
2016-04-18 17:38:28 +0100

    header for defaults that concern only tree drawing

---
+++ mfl/mfl_drawtree.h
@@ -0,0 +1,15 @@
+//
+//  mfl_drawtree.h
+//  Morphy
+//
+//  Created by mbrazeau on 18/04/2016.
+//
+//
+
+#ifndef mfl_drawtree_h
+#define mfl_drawtree_h
+
+#define MAX_BUFFER_WIDTH 80
+#define DEFAULT_TIP_COLUMN 63
+
+#endif /* mfl_drawtree_h */
