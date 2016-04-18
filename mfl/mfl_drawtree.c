14a3f63e303478b6731430fa760184b8af8dd799
mbrazeau <mbrazeau@dyn1208-62.wlan.ic.ac.uk>
2016-04-18 16:46:15 +0100

    rescales branches to number of input taxa

--- mfl/mfl_drawtree.c
+++ mfl/mfl_drawtree.c
@@ -67,9 +67,9 @@ void mfl_write_into_tipfield(char* name, char* grid, int row, int col)
 
 
 
-void mfl_printree_set_node_coordinates(mfl_node_t *n, int row)
+void mfl_printree_set_node_coordinates(mfl_node_t *n, int row, int num_taxa)
 {
-    int branchlen = 8; // TODO: Get rid of this magic number; calculate estimated tree depth
+    int branchlen = (2*63) / num_taxa; // TODO: Get rid of this magic number; calculate estimated tree depth
     mfl_node_t* p = NULL;
     mfl_node_t* leftdesc = NULL;
     mfl_node_t* rightdesc = NULL;
@@ -120,25 +120,25 @@ void mfl_drawtree_apply_subbranch(mfl_node_t* parent, mfl_node_t* desc, char *gr
     }
 }
 
-void mfl_printtree_set_coords_traversal(mfl_node_t* n, int* currentrow, char* grid)
+void mfl_printtree_set_coords_traversal(mfl_node_t* n, int* currentrow, char* grid, int num_taxa)
 {
     mfl_node_t* p;
     
     if (n->nodet_tip) {
-        mfl_printree_set_node_coordinates(n, *currentrow);
+        mfl_printree_set_node_coordinates(n, *currentrow, num_taxa);
         *currentrow = *currentrow + 2;
         return;
     }
     
     p = n->nodet_next;
     do {
-        mfl_printtree_set_coords_traversal(p->nodet_edge, currentrow, grid);
+        mfl_printtree_set_coords_traversal(p->nodet_edge, currentrow, grid, num_taxa);
 
         p = p->nodet_next;
     } while (p != n);
     
     
-    mfl_printree_set_node_coordinates(n, *currentrow);
+    mfl_printree_set_node_coordinates(n, *currentrow, num_taxa);
 }
 
 void mfl_drawtree_add_nodebar(mfl_node_t* n, mfl_node_t* ldesc, mfl_node_t* rdesc, char* grid)
@@ -196,12 +196,12 @@ void mfl_drawtree_draw_traversal(mfl_node_t* n, char *grid)
 
 void tui_test_tree_printing()
 {
+    int num_taxa = 12;
+    char *grid = mfl_drawtree_create_virtual_grid(num_taxa);
     
-    char *grid = mfl_drawtree_create_virtual_grid(7);
+    char newick[] = "[&R] = ((2,(1,(3,(9,10)))),4,(7,((5,(11,12)),(6,8))));";
     
-    char newick[] = "[&R] = ((2,1,3),4,(7,(5,6)));";
-    
-    mfl_tree_t* printme = mfl_convert_newick_to_mfl_tree_t(newick, 7);
+    mfl_tree_t* printme = mfl_convert_newick_to_mfl_tree_t(newick, num_taxa);
     
     printme->treet_treenodes[0]->nodet_tipname = (char*)"Borosaurus jibberstoni";
     printme->treet_treenodes[1]->nodet_tipname = (char*)"Wayne";
@@ -213,7 +213,7 @@ void tui_test_tree_printing()
     printme->treet_treenodes[7]->nodet_tipname = (char*)"Fubarus";
     
     int firstrow = 0;
-    mfl_printtree_set_coords_traversal(printme->treet_root, &firstrow, grid);
+    mfl_printtree_set_coords_traversal(printme->treet_root, &firstrow, grid, num_taxa);
     mfl_drawtree_draw_traversal(printme->treet_root, grid);
     //dbg_printf("________\n");
     dbg_printf("          1.........2.........3.........4.........5.........6.........7.........8\n");
