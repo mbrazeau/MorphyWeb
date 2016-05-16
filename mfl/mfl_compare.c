8fc66ce8ce277ba5c5d2531ffa634a185dbe7a6f
mbrazeau <mbrazeau@mbrazeaus-mbp.home>
2016-05-16 18:06:00 +0100

    ++templates for functions to compare two trees according to the number of shared bipartitions

--- mfl/mfl_compare.c
+++ mfl/mfl_compare.c
@@ -8,6 +8,42 @@
 
 #include "morphy.h"
 
+void mfl_sort_bipartition_set(mfl_partition_set_t* biparts)
+{
+    
+}
+
+
+int mfl_binary_search_bipartition(mfl_bitset_t* bipartition, mfl_partition_set_t* targetlist)
+{
+    return 0;
+}
+
+int mfl_compare_all_bipartitions(mfl_partition_set_t* partset1, mfl_partition_set_t* partset2)
+{
+    
+}
+
+
+mfl_partition_set_t* mfl_create_bipartition_set(int num_taxa)
+{
+    mfl_partition_set_t* newbipartset = (mfl_partition_set_t*)malloc(sizeof(mfl_partition_set_t));
+    if (!newbipartset) {
+        dbg_eprintf("unable to allocate memory for new bipartition set");
+        return NULL;
+    }
+    else {
+        memset(newbipartset, 0, sizeof(mfl_partition_set_t));
+    }
+    
+}
+
+void mfl_destroy_bipartition_set(mfl_partition_set_t* bptset)
+{
+    
+}
+
+
 /* This is mostly a temporary function, as it is likely that the setting of 
  * bipartitions will be handled simultaneously by other tree traversals. 
  */
