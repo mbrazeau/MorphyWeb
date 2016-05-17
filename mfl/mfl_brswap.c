966c5cb62a78fa1aeadeeb4e7b3351af61399f29
Thomas Guillerme <guillert@tcd.ie>
2016-05-17 14:59:23 +0100

    SBR algorithm ideas (missing most of the inner logic)

--- mfl/mfl_brswap.c
+++ mfl/mfl_brswap.c
@@ -42,6 +42,14 @@ void mfl_regrafting_traversal(mfl_node_t* n, mfl_node_t* src, mfl_searchrec_t* s
     // Traverse the target tree, attempting the reinsertion
 }
 
+// TG: same function but different arguments (we might want to modify it)
+void mfl_rebranching_traversal(mfl_treebuffer_t* tree_buffer, mfl_tree_t* rebranch_tree, mfl_tree_t* clip_tree, bool neighbour_rule)
+{
+    // Traverse the target tree (rebranch_tree), attempting the reinsertion (clip_tree) with or witout neighbour_rule
+    
+    // We might want to add a counter to know where in the buffer to store the new clipped tree?
+}
+
 
 void mfl_pruning_traversal(mfl_node_t* n, mfl_searchrec_t* searchrec)
 {
@@ -50,6 +58,66 @@ void mfl_pruning_traversal(mfl_node_t* n, mfl_searchrec_t* searchrec)
     // At each clipping:
     //      mfl_regrafting_traversal()
 }
+
+// TG: algorithmic node, will it be easier to do the whole algorithm as two nested traversal?
+mfl_treebuffer_t* algorithm_idea(mfl_tree_t* starttree, mfl_searchrec_t* searchrec, mfl_node_t *start, int* num_taxa)
+{
+    //Variables
+    int clade_counter = 0; //TG: this is the counter for applying the neigbhour rule: any subtree that is <= to clade_counter cannnot rebranch on the neigbhour edges.
+    int total_num_trees = 0;
+    mfl_tree_t* clipped_tree = NULL;
+    mfl_tree_t* branching_tree = NULL;
+    mfl_treebuffer_t* tree_buffer = NULL;
+    
+    // Allocate the right tree buffer size (which is (2*(n-3)*(2*(n-3)-1)) trees)
+    total_num_trees = (2*(starttree->treet_num_taxa-3)*(2*(starttree->treet_num_taxa-3)-1));
+    tree_buffer = mfl_alloc_treebuffer(total_num_trees);
+    
+    // Intialising the traversal on the first tip
+    mfl_node_t* current_clip = starttree->treet_treenodes[0];
+    // Checking if the first tip is actually a tip
+    if(current_clip->nodet_tip != 0) {
+        // Some error message?
+    }
+
+    // TRAVERSAL START
+    
+    
+    // Create the two different trees (branching and clip)
+    // Create the branching tree
+    branching_tree = mfl_copy_tree_topology(starttree); //TG: needs some malloc here! Also: use void mfl_disconnect_node(mfl_node_t* n), not sure how to separate the two trees though?
+    // Create the clipped tree
+    clipped_tree = mfl_copy_tree_topology(starttree); //TG: needs some malloc here!
+    
+    if (clipped_tree->treet_num_taxa > clade_counter) {
+        // rebranch everywhere
+        mfl_rebranching_traversal(tree_buffer, branching_tree, clipped_tree, 0); //TG: don't apply neighbour rule
+        // incrementing clade counter
+        ++clade_counter;
+    } else {
+        // do the traversal with neigbouring rule
+        mfl_rebranching_traversal(tree_buffer, branching_tree, clipped_tree, 1); //TG: don't apply neighbour rule
+    }
+    
+    // Move to the next node
+    current_clip = current_clip->nodet_next;
+    
+    do {
+        // Go through the next nodes and print the next tip
+//        algorithm_idea(); // TG: I think the difficulty is to make sure that the next node is never +2 taxa bigger than the previous one (to increment the clade_counter only one at the time).
+        
+        // Move to the next node
+        current_clip = current_clip->nodet_next;
+        
+        // Stop once reached back the start tree
+    } while (current_clip != starttree->treet_treenodes[0]);
+    
+    
+    return tree_buffer;
+}
+
+
+
 
 void mfl_subtree_pruning_and_regrafting(mfl_tree_t* starttree, mfl_searchrec_t* searchrec)
 {
