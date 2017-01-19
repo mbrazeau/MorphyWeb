2d3919c836c11e903c3bacdc73f80e01c36c9676
Thomas Guillerme <guillert@tcd.ie>
2017-01-19 11:01:21 +0000

    First down pass implemented

--- R/plot.algorithm.R
+++ R/plot.algorithm.R
@@ -7,14 +7,77 @@ character <- c(1,2,2,1,1) ## Inapplicable token will be converted to -1
 plot(tree) ; nodelabels(); tiplabels(character, adj = -2)
 
 states_matrix <- make.states.matrix(tree, character)
+states_matrix <- first.downpass(states_matrix, tree)
+
+#' @title Convert character
+#'
+#' @description Convert a character if it is not numeric (transforming - into -1 and ? into all characters (but - ))
+#'
+#' @param character any character vector
+#'
+#' @author Thomas Guillerme
+
+convert.char <- function(character) {
+
+    convert.inappli <- function(X) {
+        return(ifelse(X == "-", -1, X))
+    }
+
+    convert.missing <- function(X, all_states) {
+        if(X == "?") {
+            return(all_states)
+        } else {
+            return(X)
+        }
+    }
+
+    ## Character is a list
+    if(class(character) == "list") {
+        if(unique(unlist(lapply(character, class))) != "numeric") {
+            stop("Character list does not contain only numeric values.")
+        } else {
+            return(character)
+        }
+    }
+
+    ## Character is a vector (numeric)
+    if(class(character) == "numeric") {
+        return(as.list(character))
+    }
+    
+    ## Character is not numeric
+    if(class(character) == "character") {
+        if(length(character) == 1) {
+            #Split the character chain
+            character <- as.character(strsplit(as.character(character), "")[[1]])
+        } 
+        ## Convert into list
+        character <- as.list(character)
+
+        ## Get all states
+        all_states <- as.numeric(character)
+        all_states <- all_states[-c(which(is.na(all_states)), which(all_states == -1))]
+
+        ## Convert inapplicables
+        character <- lapply(character, convert.inappli)
+        
+        ## Convert missing
+        character <- lapply(character, convert.missing, all_states)
+
+        ## Convert into numeric
+        return(lapply(character, as.numeric))
+    }
+}
 
 #' @title Sates matrix
 #'
 #' @description Creates a states matrix
 #'
 #' @param tree \code{phylo}, a tree
-#' @param character \code{character}, a vector of character states
+#' @param character Either vector of character states (\code{"numeric"} or \code{"character"}) or a list of the same length of than the tips in the tree (see details)
 #'
+#' @details
+#' If \code{character} argument is a list, each element of the list must be a \code{"numeric"} vector with \code{"?"} being all states and \code{"-"} being \code{-1}.
 #' @author Thomas Guillerme
 
 make.states.matrix <- function(tree, character) {
@@ -24,16 +87,14 @@ make.states.matrix <- function(tree, character) {
         stop("The tree must be of class 'phylo'.")
     }
 
+    ## Transform character
+    if(class(character) != "list") {
+        character <- convert.char(character)
+    }
+
     ## Check if the character is the same length as the tree
     if(Ntip(tree) != length(character)) {
         stop("The tree and character arguments don't match.")
-    }
-
-    ## Transform character
-    if(class(character) != "numeric") {
-        #character <- convert.char(character)
-    } else {
-        character <- as.list(character)
     }
 
     ## Set up the list of characters
@@ -61,22 +122,43 @@ desc.anc <- function(node, tree) {
     return(c(descendants, ancestor))
 }
 
-
-#' @title Convert character
-#'
-#' @description Convert a character if it is not numeric (transforming - into -1 and ? into all characters (but - ))
-#'
-#' @param character any character vector
-#'
-#' @author Thomas Guillerme
-
-convert.char <- function(character) {
-    ## Convert - into -1
-    ## Convert ? into 0:n
-    ## Convert 01 into c(01)
-    #as.numeric(strsplit(as.character(character), "")[[1]])
+## Get an union (&)
+get.union <- function(a, b) {
+    options(warn = -1)
+    if(!any(a == b)) {
+        ## No union
+        return(FALSE)
+    } else {
+        ## Union
+        if(length(a) >= length(b)) {
+            return(a[which(a == b)])
+        } else {
+            return(b[which(b == a)])
+        }
+    }
+    options(warn = 0)
 }
 
+## Get an intersection inclusive (|)
+get.intersection.incl <- function(a, b) {
+    out <- unique(c(a,b))
+    if(length(out) == 0) {
+        return(FALSE)
+    } else {
+        return(out)
+    }
+}
+
+## Get an intersection exclusive (^)
+get.intersection.excl <- function(a, b) {
+    out <- get.intersection.incl(a,b)
+    out <- out[-which(out == get.union(a,b))]
+    if(length(out) == 0) {
+        return(FALSE)
+    } else {
+        return(out)
+    }
+}
 
 
 #' @title First downpass
@@ -98,23 +180,32 @@ first.downpass <- function(states_matrix, tree) {
 
         ## Select the descendants and ancestors
         desc_anc <- desc.anc(node, tree)
+        right <- states_matrix$Dp1[desc_anc[1]][[1]]
+        left <- states_matrix$Dp1[desc_anc[2]][[1]]
 
-        ## 
-        if(states_matrix$Dp1[desc_anc[1]][[1]] && states_matrix$Dp1[desc_anc[2]][[1]]) {
-            states_matrix$Dp1[[node]] <- states_matrix$Dp1[desc_anc[1]][[1]][which(states_matrix$Dp1[desc_anc[1]][[1]] == states_matrix$Dp1[desc_anc[2]][[1]])]
+        ## Get the union between the descendants
+        union_desc <- get.union(left, right)
+
+        if(union_desc != FALSE) {
+            ## If there is a union, set the node to be the union
+            states_matrix$Dp1[[node]] <- union_desc
+
+            ## If the union is actually the inapplicable token, and both descendants have an applicable, set it to be the intersection between applicable
+            if(union_desc == -1 && any(left != -1) && any(right != -1)) {
+                states_matrix$Dp1[[node]] <- get.intersection.incl(left[-which(left == -1)], right[-which(right == -1)])
+            }
         } else {
-            
+            ## Else set it to be the intersection
+            states_matrix$Dp1[[node]] <- get.intersection.incl(left, right)
+
+            ## If the node has inapplicable data
+            if(any(states_matrix$Dp1[[node]] == -1)) {
+                ## If both left and right have applicable, set the node to be applicable only
+                if(any(left != -1) && any(right != -1)) {
+                    states_matrix$Dp1[[node]] <- states_matrix$Dp1[[node]][-which(states_matrix$Dp1[[node]] == -1)]
+                }
+            }
         }
-
-            
-
-            states_matrix[desc_anc[1], 2] & states_matrix[desc_anc[2], 2]
-
-
-            )
-
-
-        cat(node)
     }
 
     return(states_matrix)
@@ -239,3 +330,4 @@ plot.inapplicable.algorithm <- function(tree, character, n_passes = 4, ...) {
     return(invisible())
 }
 
+    
--- R/test-plot.algorithm.R
+++ R/test-plot.algorithm.R
@@ -2,6 +2,24 @@ library(testthat)
 
 context("utilities")
 
+test_that("convert.char works", {
+    ## Simple case
+    character <- list(1,2,3,-1,c(1,2,3))
+    expect_equal(convert.char(character), character)
+
+    ## Numeric case
+    char_num <- c(1,2,3,4,5)
+    expect_equal(convert.char(char_num), as.list(char_num))
+
+    ## Character case 1
+    char_char <- c("1", "2", "3", "-", "?")
+    expect_equal(convert.char(char_char), character)
+
+    ## Character case 2
+    char_char <- c("123-?")
+    expect_equal(convert.char(char_char), character)
+})
+
 test_that("make.states.matrix works", {
     ## Simple case
     tree <- rtree(5)
@@ -19,6 +37,13 @@ test_that("make.states.matrix works", {
     expect_equal(unique(unlist(lapply(test, length))), 9)
     expect_equal(unlist(test$Char), rep(1, 5))
     expect_equal(names(test), c("Char", "Dp1", "Up1", "Dp2", "Up2"))
+
+    ## Good functioning with characters
+    test <- make.states.matrix(tree, "123-?")
+    expect_is(test, "list")
+    expect_equal(length(test), 5)
+    expect_equal(unique(unlist(lapply(test, length))), 9)
+    expect_equal(unlist(test$Char), c(1,2,3,-1,1,2,3))
 })
 
 test_that("desc.anc works", {
@@ -31,4 +56,20 @@ test_that("desc.anc works", {
     expect_equal(desc.anc(7, tree), c(1,2,6))
     expect_equal(desc.anc(8, tree), c(3,9,6))
     expect_equal(desc.anc(9, tree), c(4,5,8))
+})
+
+
+test_that("logicals work", {
+    ## Union
+    expect_false(get.union(1,2))
+    expect_equal(get.union(1,1), 1)
+    expect_equal(get.union(c(1),c(1,2,3)), 1)
+
+    ## Intersection.incl
+    expect_equal(get.intersection.incl(1,1), 1)
+    expect_equal(get.intersection.incl(c(1),c(1,2,3)), c(1,2,3))
+
+    ## Intersection.excl
+    expect_false(get.intersection.excl(1,1))
+    expect_equal(get.intersection.excl(c(1),c(1,2,3)), c(2,3))
 })
